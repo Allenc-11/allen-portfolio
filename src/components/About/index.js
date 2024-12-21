4f24d6203ff026079066696ddc397b0f88fd843d
Allen <129023261+Allenc-11@users.noreply.github.com>
2024-12-21 15:42:43 -0500

    About page
    added an about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,90 @@
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faNodeJs,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    // Cleanup function
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
   return (
-    <div className="about-container">
-      <h1>hello</h1>
-    </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a very ambitious Computer Science student at Toronto
+            Metropolitan University with a deep interest in software
+            development. Alongside my studies, I’m pursuing a minor in
+            mathematics to strengthen my analytical thinking and problem-solving
+            skills.
+          </p>
+          <p>
+            I’ve built a solid foundation in programming languages such as Java,
+            Python, and C/C++, and I’m steadily expanding my skills in front-end
+            technologies like HTML/CSS, JavaScript, React.js and Node.js to
+            create dynamic and user-friendly web applications. Each project I
+            take on is an opportunity to learn and grow, and I’m genuinely
+            excited to tackle new challenges that push me to improve.
+          </p>
+          <p>
+            Fluent in both English and Chinese, I’m actively exploring co-op or
+            internship opportunities where I can apply my technical knowledge,
+            contribute to impactful projects, and continue growing in real-world
+            work settings.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faNodeJs} color="#215732" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4B28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
