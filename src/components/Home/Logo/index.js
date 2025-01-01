58f434e8423e27930524fb08f119cd654cad1d14
Allen <129023261+Allenc-11@users.noreply.github.com>
2025-01-01 17:56:04 -0500

    added logo and started portfolio page
    added a picture logo to home page as well as a motion circle; also starting developing the portfolio page

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,10 +1,39 @@
 import './index.scss'
-import LogoS from '../../../assets/images/logo-a3d.png'
+import { motion } from 'framer-motion'
+import Photo from '../../../assets/images/photo.png'
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <img className="solid-logo" src={LogoS} alt="A" />
+      <img className="solid-logo" src={Photo} alt="developer photo" />
+      <img className="blur-logo" src={Photo} alt="" />
+      <motion.svg
+        width="506"
+        height="506"
+        viewBox="0 0 506 506"
+        fill="transparent"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.circle
+          cx="253"
+          cy="253"
+          r="250"
+          stroke="#00ff99"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          initial={{ strokeDasharray: '24 10 0 0' }}
+          animate={{
+            strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
+            rotate: [120, 360],
+          }}
+          transition={{
+            duration: 20,
+            repeat: Infinity,
+            repeatType: 'reverse',
+          }}
+        />
+      </motion.svg>
     </div>
   )
 }
